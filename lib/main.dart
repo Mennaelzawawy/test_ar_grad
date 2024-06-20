f2b51a5b3dab01de8e3ff500b3346d19cd064f8b
Mennaelzawawy <77809699+Mennaelzawawy@users.noreply.github.com>
2024-06-20 18:15:52 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
+
+void main() {
+  runApp(ATestApp());
+}
+
+class ATestApp extends StatefulWidget {
+  const ATestApp({super.key});
+
+  @override
+  State<ATestApp> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<ATestApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: ModelViewer(
+          src: 'assets/models/out (13).glb',
+          ar: true,
+          arPlacement: ArPlacement.floor,
+          autoRotate: true,
+          cameraControls: true,
+        ),
+      ),
+    );
+  }
+}
